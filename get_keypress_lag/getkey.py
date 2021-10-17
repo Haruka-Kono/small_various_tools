201a975640fab521a4b068df733ecabc05bcebab
Haruka-Kono <kono.h.ab@m.titech.ac.jp>
2021-10-17 19:01:04 +0900

    update

--- get_keypress_lag/getkey.py
+++ get_keypress_lag/getkey.py
@@ -5,11 +5,12 @@ import datetime
 import numpy as np
 import pandas as pd
 
+COUNTS = []
 KEYS = []  # 入力キーリスト
 RELEASE_LAGS = []  # タイムラグリスト
-COUNTS = []
 REACT_LAGS = []
-
+DAMAGE_ROW = []
+DLEVEL = []
 i = 0
 req_char = 'p'  # 入力要求文字
 p = 'press "p"'
@@ -22,6 +23,12 @@ now = datetime.datetime.now()
 print(now)
 
 print('画面の指示に従ってp, q, o, wを打ってください。各15回、計60回。多いけど頑張りましょう。p, qは小指、o, wは薬指を使ってください。')
+
+print('これから打つのはpです。現在の指の疲労感（≒今日のPC作業量）はどれくらいですか？ 1 (ほとんど疲れていない) ~5 (非常に疲れている)の中から選んでください。')
+damage = keyboard.read_key()
+DAMAGE_ROW.append('pre_damege_p')
+DLEVEL.append(damage)
+
 time.sleep(1)
 print(p)
 display = time.perf_counter()
@@ -50,24 +57,59 @@ while keyboard.read_key() != 'esc':  # esc押すまでwhileブロック内の処
     if i == 15:
         req_char = 'q'
         message = q
+        print('現在の疲労感はどれくらいですか？ 1 (ほとんど疲れていない) ~5 (非常に疲れた)の中から選んでください。※途中から別の指で打った場合は0を選んでください。')
+        damage = keyboard.read_key()
+        DAMAGE_ROW.append('post_damege_p')
+        DLEVEL.append(damage)
+
         print('-----次はqです-----')
+        print('現在の指の疲労感（≒今日のPC作業量）はどれくらいですか？ 1 (ほとんど疲れていない) ~5 (非常に疲れている)の中から選んでください。')
+        damage = keyboard.read_key()
+        DAMAGE_ROW.append('pre_damege_q')
+        DLEVEL.append(damage)
         time.sleep(1)
+
     if i == 30:
         req_char = 'o'
         message = o
+        print('疲労感はどれくらいですか？ 1 (ほとんど疲れていない) ~5 (非常に疲れた)の中から選んでください。※途中から別の指で打った場合は0を選んでください。')
+        damage = keyboard.read_key()
+        DAMAGE_ROW.append('post_damege_q')
+        DLEVEL.append(damage)
+
         print('-----次はoです-----')
+        print('現在の指の疲労感（≒今日のPC作業量）はどれくらいですか？ 1 (ほとんど疲れていない) ~5 (非常に疲れている)の中から選んでください。')
+        damage = keyboard.read_key()
+        DAMAGE_ROW.append('pre_damege_o')
+        DLEVEL.append(damage)
         time.sleep(1)
     if i == 45:
         req_char = 'w'
         message = w
+        print('疲労感はどれくらいですか？ 1 (ほとんど疲れていない) ~5 (非常に疲れた)の中から選んでください。※途中から別の指で打った場合は0を選んでください。')
+        damage = keyboard.read_key()
+        DAMAGE_ROW.append('post_damege_q')
+        DLEVEL.append(damage)
+
         print('-----次はwです-----')
+        print('現在の指の疲労感（≒今日のPC作業量）はどれくらいですか？ 1 (ほとんど疲れていない) ~5 (非常に疲れている)の中から選んでください。')
+        damage = keyboard.read_key()
+        DAMAGE_ROW.append('pre_damege_w')
+        DLEVEL.append(damage)
         time.sleep(1)
 
     time.sleep(0.5)
     print(message)
     display = time.perf_counter()
+
     if i == 60:
+        print('疲労感はどれくらいですか？ 1 (ほとんど疲れていない) ~5 (非常に疲れた)の中から選んでください。※途中から別の指で打った場合は0を選んでください。')
+        damage = keyboard.read_key()
+        DAMAGE_ROW.append('post_damege_w')
+        DLEVEL.append(damage)
         break
+
+
 
 # keys_array = np.array(KEYS)
 # lags_array = np.array(RELEASE_LAGS)
@@ -81,5 +123,11 @@ if i > 30:
     now_prefix = now.strftime('%Y-%m-%d_%Hh%Mm')
     #print(now_prefix)
     #print(now_prefix+'_test')
-    df.to_csv(now_prefix+'_typedata.csv', index=None,)
+    df.to_csv(now_prefix+'_typedata.csv', index=None)
     print(df)
+
+    damage_data = np.stack([DAMAGE_ROW, DLEVEL])
+    df_d = pd.DataFrame(damage_data)
+    df_d = df_d.T
+    df_d.to_csv(now_prefix+'_damage.csv', index=None)
+    print(df_d)
