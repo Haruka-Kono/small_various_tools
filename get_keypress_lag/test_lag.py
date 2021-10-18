aa0d520efd79dd1aa1cfd9aa748802ee731d881c
Haruka-Kono <kono.h.ab@m.titech.ac.jp>
2021-10-18 13:16:33 +0900

    update

--- get_keypress_lag/test_lag.py
+++ get_keypress_lag/test_lag.py
@@ -1,7 +1,77 @@
-#import time
+# -*- coding: utf-8 -*-
+import keyboard
+import time
 import datetime
+import numpy as np
+import pandas as pd
+
+COUNTS = []
+KEYS = []  # 入力キーリスト
+RELEASE_LAGS = []  # タイムラグリスト
+REACT_LAGS = []
+CHAR = []
+DLEVEL_PRE = []
+DLEVEL_POST = []
+i = 0
+req_char = 'p'  # 入力要求文字
+
+p = 'press "p"'
+semic = 'press ";"'
+
+q = 'press "q"'
+a = 'press "a"'
+
+o = 'press "o"'
+el = 'press "l"'
+
+w = 'press "w"'
+s = 'press "s"'
+message = p  # 入力キー初期値
+
 now = datetime.datetime.now()
 print(now)
-now_prefix = now.strftime('%Y-%m-%d_%Hh%Mm')
-print(now_prefix)
-print(now_prefix+'_test')
+
+print('画面の指示に従ってキーを打ってください。計80回。多いけど頑張りましょう。p/;, q/aは小指、o/l, w/sは薬指を使ってください。')
+
+print('これから打つのはp/;です。右手小指で打ってください。現在の右手小指の疲労感（≒今日のPC作業量）はどれくらいですか？ 1 (ほとんど疲れていない) ～ 5 (非常に疲れている)の中から選んでください。')
+damage = input()
+# print(damage)
+CHAR.append('p')
+DLEVEL_PRE.append(damage)
+
+print('-----まもなくp開始です-----')
+time.sleep(1)
+print(p)
+display = time.perf_counter()
+
+# while i<21:
+while keyboard.read_key() != 'esc':  # esc押すまでwhileブロック内の処理実行。その分なんかしら押さないと始まらない
+    # print('p')
+    press = time.perf_counter()
+    key = keyboard.read_key()
+    release = time.perf_counter()
+
+    if 0 < i < 20 and i % 2 == 0:
+        req_char = 'p'
+        message = p
+    else 0 < i < 20 and i % 2 != 0:
+        req_char = ';'
+        message = semic
+
+    if key == req_char:
+        i += 1
+        release_lag = release-press
+        react_lag = press-display
+        KEYS.append(key)
+        COUNTS.append(i)
+        RELEASE_LAGS.append(release_lag)
+        REACT_LAGS.append(react_lag)
+
+        print(COUNTS)
+        print(KEYS)
+        print(RELEASE_LAGS)
+        print(REACT_LAGS)
+
+    time.sleep(0.5)
+    print(message)
+    display = time.perf_counter()
