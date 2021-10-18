a1a6c1e03fd41497a29789ac2302688fdeb57e9a
Haruka-Kono <kono.h.ab@m.titech.ac.jp>
2021-10-18 20:48:03 +0900

    update

--- get_keypress_lag/test_lag.py
+++ get_keypress_lag/test_lag.py
@@ -32,7 +32,6 @@ now = datetime.datetime.now()
 print(now)
 
 print('画面の指示に従ってキーを打ってください。計80回。多いけど頑張りましょう。p/;, q/aは小指、o/l, w/sは薬指を使ってください。')
-
 print('これから打つのはp/;です。右手小指で打ってください。現在の右手小指の疲労感（≒今日のPC作業量）はどれくらいですか？ 1 (ほとんど疲れていない) ～ 5 (非常に疲れている)の中から選んでください。')
 damage = input()
 # print(damage)
@@ -45,16 +44,18 @@ print(p)
 display = time.perf_counter()
 
 # while i<21:
-while keyboard.read_key() != 'esc':  # esc押すまでwhileブロック内の処理実行。その分なんかしら押さないと始まらない
+## esc押すまでwhileブロック内の処理実行。その分なんかしら押さないと始まらない
+while keyboard.read_key() != 'esc':
     # print('p')
     #    if key == req_char and i == 0:
     #        i += 1
     #        continue
-
+    ## 入力キー取得、押下終了時間取得
     press = time.perf_counter()
     key = keyboard.read_key()
     release = time.perf_counter()
 
+    ## 入力キー正誤判定及びリスト追加（and i > 0はいらないか？）、表示
     if key == req_char and i > 0:
         release_lag = release-press
         react_lag = press-display
@@ -68,21 +69,40 @@ while keyboard.read_key() != 'esc':  # esc押すまでwhileブロック内の処
         print(REACT_LAGS)
         i += 1
 
+    ## 入力要求文字分岐
     if 1 < i < 21 and i % 2 != 0:
         req_char = 'p'
         message = p
-
-    if 1 < i < 21 and i % 2 == 0:
+    if 1 < i < 20 and i % 2 == 0:
         req_char = ';'
         message = semic
 
-    time.sleep(0.5)
-    print(message)
-    display = time.perf_counter()
+    if 21 < i 40 and i % 2 != 0:
+        req_char = 'q'
+        message = q
+    if 21 < i 40 and i % 2 == 0:
+        req_char = 'a'
+        message = a
 
+    ## 各指入力終了時の処理
     if i == 21:
-        print('pは終了です。現在の疲労感はどれくらいですか？ 1 (ほとんど疲れていない) ～ 5 (非常に疲れている)の中から選んでください。※途中から別の指で打った場合は0を選んでください。')
+        print('p/;は終了です。現在の疲労感はどれくらいですか？ 1 (ほとんど疲れていない) ～ 5 (非常に疲れている)の中から選んでください。※途中から別の指で打った場合は0を選んでください。')
         damage = input()
     # print(damage)
     # CHAR.append('p')
         DLEVEL_POST.append(damage)
+        req_char = 'q'
+        message = q
+        print('-----次はq/aです。左手小指で打ってください-----')
+        print('現在の左手小指の疲労感（≒今日のPC作業量）はどれくらいですか？ 1 (ほとんど疲れていない) ～ 5 (非常に疲れている)の中から選んでください。')
+        damage = input()
+        # print(damage)
+        CHAR.append('q')
+        DLEVEL_PRE.append(damage)
+        print('-----まもなくq開始です-----')
+        time.sleep(1)
+
+    ## 入力文字表示、反応時間測定開始（原則的に最後部）
+    time.sleep(0.5)
+    print(message)
+    display = time.perf_counter()
