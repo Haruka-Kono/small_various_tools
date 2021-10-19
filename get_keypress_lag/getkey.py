44d1f287d57722fb07655d045ea243a191af24ff
Haruka-Kono <kono.h.ab@m.titech.ac.jp>
2021-10-19 20:58:07 +0900

    fix getting datetime

--- get_keypress_lag/getkey.py
+++ get_keypress_lag/getkey.py
@@ -29,10 +29,12 @@ s = 'press "s"'
 
 message = p  # 入力キー初期値
 
-now = datetime.datetime.now('%Y-%m-%d %H:%Mm')
+now = datetime.datetime.now()
+now.strftime('%Y-%m-%d %H:%Mm')
 print('現在時刻: '+now)
 
 print('画面の指示に従ってキーを打ってください。計80回。多いけど頑張りましょう。p/;, q/aは小指、o/l, w/sは薬指を使ってください。')
+time.sleep((1))
 print('これから打つのはp/;です。右手小指で打ってください。現在の右手小指の疲労感（≒今日のPC作業量）はどれくらいですか？ 1 (ほとんど疲れていない) ～ 5 (非常に疲れている)の中から選んでください。')
 damage = input()
 # print(damage)
